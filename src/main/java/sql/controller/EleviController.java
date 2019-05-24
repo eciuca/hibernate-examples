dc8c4b99ccae08e36ed51f4eca5de1d27e7cac72
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2019-05-24 20:55:05 +0300

    Am creat un controller pentru elevi

--- src/main/java/sql/controller/EleviController.java
+++ src/main/java/sql/controller/EleviController.java
@@ -1,5 +1,8 @@
 package sql.controller;
 
+import sql.entities.Elev;
+import sql.service.EleviService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,12 +14,44 @@ import java.io.PrintWriter;
 @WebServlet(urlPatterns = "/elevi")
 public class EleviController extends HttpServlet {
 
+    private final EleviService eleviService;
+
+    public EleviController() {
+        this.eleviService = new EleviService();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         PrintWriter writer = resp.getWriter();
 
-        writer.println("Hello");
+
+
+
+        writer.println("<html><head></head><body>");
+        writer.println("<p>Hello</p>");
+
+        String id= req.getParameter("id");
+        if (id != null && !id.isEmpty()) {
+            writer.println("<span>");
+            writer.println(eleviService.findElevById(Integer.parseInt(id)));
+            writer.println("</span>");
+        } else {
+            writer.println("<ul>");
+            for (Elev elev : eleviService.findAllElevi()) {
+                writer.println("<li>" + elev.toString() + "</li>");
+            }
+        }
+        writer.println("</ul>");
+        writer.println("</body></html>");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String nume = req.getParameter("nume");
+        int idClasa = 1;
+
+        eleviService.addElev(nume, 1);
 
     }
 }
